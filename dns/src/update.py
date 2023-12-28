17574df1e6788cae4cf82f9a682785edb7a505e8
Adam Żmuda <adzik007@gmail.com>
2023-12-28 10:39:17 +0100

    move dns to class

--- dns/src/update.py
+++ dns/src/update.py
@@ -3,7 +3,7 @@ import logging
 
 from public_ip import PublicIP
 
-import dns
+from dns import HandlerDNS
 
 
 def get_config(config_path: str) -> dict:
@@ -26,23 +26,16 @@ current_ip = ip_handler.get_public_ip()
 previous_ip = ip_handler.get_previous_public_ip()
 
 if current_ip == previous_ip:
-    logging.info(f"IP Address unchanged")
+    logging.info("IP Address unchanged")
     quit(0)
 
-dns_token = dns.get_dns_token(
-    username=secrets["username"],
-    password=secrets["password"],
-    device_token=secrets["device_token"]
-)
+dns_handler = HandlerDNS(template_path="../config/payload_template.json", secrets=secrets, config=config)
 
 for entry in config["records"]:
-    payload = dns.get_payload(
-        template_path="../config/payload_template.json",
+    payload = dns_handler.get_payload(
         public_ip=current_ip, record_id=entry["id"],
     )
-    update_dns_status = dns.update_dns_entry(
-        bearer_token=dns_token,
-        device_token=secrets["device_token"],
+    update_dns_status = dns_handler.update_dns_entry(
         payload=payload,
     )
     logging.info(f"Updated DNS record. EntryID: {entry['id']}. PublicIP: {current_ip}")
