5875be77954c6e0a422c1f93d4c30851b18bd83c
Adam Żmuda <adzik007@gmail.com>
2024-01-03 16:03:02 +0100

    add vw backup

---
+++ backup/vaultwarden.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# https://github.com/dani-garcia/vaultwarden/wiki/Backing-up-your-vault
+
+BACKUP_FILE="vaultwarden-$(date "+%F--%H%M")"
+
+sqlite3 /data/db.sqlite3 ".backup '/tmp/db.sqlite3'"
+tar -czf - tmp/db.sqlite3 data/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /backup/${BACKUP_FILE}.tar.gz
