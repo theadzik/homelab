9c2e5c55227e49dc5998db20bf751dde66d5ee4f
Adam Żmuda <adzik007@gmail.com>
2024-01-11 20:08:45 +0100

    noip handler

--- dns/src/dns.py
+++ dns/src/dns.py
@@ -1,83 +1,43 @@
 import json
 import logging
-
 import requests
+import os
+from base64 import b64encode
 
 
 class HandlerDNS:
-    def __init__(self, template_path: str, secrets: dict, config: dict):
-        self.base_url = "https://www.spaceship.com"
-        self.template_path = template_path
-        self.config = config
-        self.secrets = secrets
-        self.cookies = self.get_cookies()
-        self.headers = self.get_headers()
-
-    def get_payload(self, public_ip: str, record_id: str) -> dict:
-        with open(self.template_path, "r") as payload_template:
-            payload = json.load(payload_template)
-
-        payload["recordsToUpdate"][record_id]["address"] = public_ip
-        logging.debug(f"Generated payload={payload}")
-        return payload
-
-    def get_cookies(self) -> dict:
-        cookies = {
-            "z-account-deviceid": self.secrets['device_token'],
-        }
-        return cookies
+    def __init__(self, public_ip: str):
+        self.username = os.environ["USERNAME"]
+        self.password = os.environ["PASSWORD"]
+        self.hostname = os.environ["HOSTNAME"]
+        self.maintainer_email = os.environ["MAINTAINER_EMAIL"]
+        self.public_ip = public_ip
+        self.update_url = (
+            "https://dynupdate.no-ip.com/nic/update"
+            f"?hostname={self.hostname}"
+            f"&myip={self.public_ip}"
+        )
 
     def get_headers(self) -> dict:
+        basic_auth = b64encode(f"{self.username}:{self.password}".encode()).decode()
         headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.get_dns_token()}",
+            "Authorization": f"Basic {basic_auth}",
+            "User-Agent": f"Homelab DNS-Updater/Docker-3.0.0 {self.maintainer_email}"
         }
         return headers
 
-    def get_dns_token(self) -> str:
-        login_api = "/connect/token"
-
-        params = {
-            "client_id": "spaceship",
-            "grant_type": "password",
-            "username": self.secrets['username'],
-            "password": self.secrets['password'],
-        }
-
-        login_response = requests.request(
-            method="POST",
-            url=self.base_url + login_api,
-            data=params,
-            cookies=self.cookies
-        )
-
-        try:
-            login_response.raise_for_status()
-            bearer_token = json.loads(login_response.text)["access_token"]
-            logging.debug("Got bearer token")
-            return bearer_token
-        except Exception as e:
-            logging.error(
-                f"Failed to get bearer token. "
-                f"Status code {login_response.status_code}, Response: {login_response.text}"
-            )
-            raise e
-
-    def update_dns_entry(self, payload: dict):
-        update_api = "/gateway/api/v1/advanceddnsbff/dnsrecords/bulkUpdate"
+    def update_dns_entry(self) -> bool:
         update = requests.request(
-            method="POST",
-            url=self.base_url + update_api,
-            headers=self.headers,
-            json=payload,
-            cookies=self.cookies,
+            method="GET",
+            url=self.update_url,
+            headers=self.get_headers()
         )
 
         try:
             update.raise_for_status()
-        except Exception as e:
+        except requests.HTTPError as e:
             logging.error(f"Failed to update dns. Status code {update.status_code}, Response: {update.text}")
             raise e
 
         logging.debug(f"Updated DNS. Status code {update.status_code}, Response: {update.text}")
-        return update.json()
+        return update.ok
--- dns/src/update.py
+++ dns/src/update.py
@@ -1,4 +1,3 @@
-import json
 import logging
 from os import environ
 
@@ -17,17 +16,8 @@ previous_ip = public_ip.get_previous_public_ip()
 
 if current_ip == previous_ip:
     logging.info("IP Address unchanged")
-    quit(0)
+    #quit(0)
 
-# dns_handler = HandlerDNS(template_path="/config/payload_template.json", secrets=secrets, config=config)
-#
-# for entry in config["records"]:
-#     payload = dns_handler.get_payload(
-#         public_ip=current_ip, record_id=entry["id"],
-#     )
-#     update_dns_status = dns_handler.update_dns_entry(
-#         payload=payload,
-#     )
-# logging.info(f"Updated DNS record. EntryID: {entry['id']}. PublicIP: {current_ip}")
-# logging.debug(update_dns_status)
+dns_handler = HandlerDNS(public_ip=current_ip)
+dns_handler.update_dns_entry()
 public_ip.save_public_ip(public_ip=current_ip)
