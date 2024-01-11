1973586557dc97721561b0ec603829fe7f7b7bc6
Adam Żmuda <adzik007@gmail.com>
2024-01-11 19:20:16 +0100

    check multiple sources for public ip

--- dns/src/public_ip.py
+++ dns/src/public_ip.py
@@ -1,33 +1,40 @@
 import logging
-
+from os import environ
 import requests
 
 
-class PublicIP:
-    def __init__(self, config: dict):
-        self.config = config
-
-    @staticmethod
-    def get_public_ip() -> str:
-        ip_url = "https://ifconfig.me"
+def get_public_ip() -> str:
+    remote_ip_detection_hosts = [
+        "https://ifconfig.me",
+        "https://ipinfo.io/ip",
+        "https://ipecho.net/plain",
+        "http://ip1.dynupdate.no-ip.com",
+    ]
+    for ip_url in remote_ip_detection_hosts:
+        logging.debug(f"Requesting IP from {ip_url}")
         public_ip = requests.request("GET", ip_url)
         try:
             public_ip.raise_for_status()
-            logging.info(f"Current Public IP - {public_ip.text}")
+            logging.info(f"Current Public IP: {public_ip.text}")
             return public_ip.text
-        except Exception as e:
-            logging.error(f"Failed getting Public IP. Status code {public_ip.status_code}, Response: {public_ip.text}")
-            raise e
+        except requests.HTTPError:
+            logging.error(
+                f"Failed do get IP from {ip_url}.\n"
+                f"Status code: {public_ip.status_code}\n"
+                f"Response: {public_ip.text}"
+            )
 
-    def save_public_ip(self, public_ip: str) -> None:
-        log_path = self.config["ip_history_path"]
-        with open(log_path, "a") as ip_log:
-            ip_log.write(public_ip + "\n")
 
-    def get_previous_public_ip(self) -> str:
-        log_path = self.config["ip_history_path"]
-        try:
-            with open(log_path, "r") as ip_log:
-                return list(ip_log)[-1].strip()
-        except FileNotFoundError:
-            return ""
+def save_public_ip(public_ip: str) -> None:
+    log_path = environ["IP_HISTORY_PATH"]
+    with open(log_path, "a") as ip_log:
+        ip_log.write(public_ip + "\n")
+
+
+def get_previous_public_ip() -> str:
+    log_path = environ["IP_HISTORY_PATH"]
+    try:
+        with open(log_path, "r") as ip_log:
+            return list(ip_log)[-1].strip()
+    except FileNotFoundError:
+        return ""
--- dns/src/update.py
+++ dns/src/update.py
@@ -1,43 +1,33 @@
 import json
 import logging
+from os import environ
 
-from public_ip import PublicIP
+import public_ip
 
 from dns import HandlerDNS
-
-
-def get_config(config_path: str) -> dict:
-    with open(config_path, "r") as config_file:
-        return json.load(config_file)
-
-
-secrets = get_config("/config/secrets.json")
-config = get_config("/config/config.json")
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s: %(message)s',
     encoding='utf-8',
-    level=config["log_level"]
+    level=environ["LOG_LEVEL"]
 )
 
-ip_handler = PublicIP(config)
-
-current_ip = ip_handler.get_public_ip()
-previous_ip = ip_handler.get_previous_public_ip()
+current_ip = public_ip.get_public_ip()
+previous_ip = public_ip.get_previous_public_ip()
 
 if current_ip == previous_ip:
     logging.info("IP Address unchanged")
     quit(0)
 
-dns_handler = HandlerDNS(template_path="/config/payload_template.json", secrets=secrets, config=config)
-
-for entry in config["records"]:
-    payload = dns_handler.get_payload(
-        public_ip=current_ip, record_id=entry["id"],
-    )
-    update_dns_status = dns_handler.update_dns_entry(
-        payload=payload,
-    )
-    logging.info(f"Updated DNS record. EntryID: {entry['id']}. PublicIP: {current_ip}")
-    logging.debug(update_dns_status)
-    ip_handler.save_public_ip(public_ip=current_ip)
+# dns_handler = HandlerDNS(template_path="/config/payload_template.json", secrets=secrets, config=config)
+#
+# for entry in config["records"]:
+#     payload = dns_handler.get_payload(
+#         public_ip=current_ip, record_id=entry["id"],
+#     )
+#     update_dns_status = dns_handler.update_dns_entry(
+#         payload=payload,
+#     )
+# logging.info(f"Updated DNS record. EntryID: {entry['id']}. PublicIP: {current_ip}")
+# logging.debug(update_dns_status)
+public_ip.save_public_ip(public_ip=current_ip)
