fbe5653992bff3b8aa1dbfc5b3bb52aad7e78d6d
Adam Żmuda <adzik007@gmail.com>
2024-02-19 18:47:38 +0100

    dns-updater now works with no-ip (#6)
    * check multiple sources for public ip
    
    * noip handler
    
    * better user-agent
    
    * compare to resolved IP
    
    * added error handling
    
    * added sending email
    
    * dns updater 3.2.2
    
    * cronjob -> deployment
    
    * only save public ip if history doesn't match reality
    
    * add secret with env values
    
    * add push commands for easy copy-paste

--- dns/src/public_ip.py
+++ dns/src/public_ip.py
@@ -1,33 +1,59 @@
 import logging
+import socket
+from os import environ
 
 import requests
 
 
-class PublicIP:
-    def __init__(self, config: dict):
-        self.config = config
-
-    @staticmethod
-    def get_public_ip() -> str:
-        ip_url = "https://ifconfig.me"
-        public_ip = requests.request("GET", ip_url)
+def get_public_ip() -> str:
+    remote_ip_detection_hosts = [
+        "https://ifconfig.me",
+        "https://ipinfo.io/ip",
+        "https://ipecho.net/plain",
+        "http://ip1.dynupdate.no-ip.com",
+    ]
+    for ip_url in remote_ip_detection_hosts:
+        logging.debug(f"Requesting IP from {ip_url}")
         try:
+            public_ip = requests.request("GET", ip_url)
             public_ip.raise_for_status()
-            logging.info(f"Current Public IP - {public_ip.text}")
+            logging.debug(f"Current Public IP: {public_ip.text}")
             return public_ip.text
+        except requests.HTTPError:
+            logging.warning(
+                f"Failed do get IP from {ip_url}.\n"
+                f"Status code: {public_ip.status_code}\n"
+                f"Response: {public_ip.text}"
+            )
         except Exception as e:
-            logging.error(f"Failed getting Public IP. Status code {public_ip.status_code}, Response: {public_ip.text}")
-            raise e
+            logging.warning(
+                f"Failed do get IP from {ip_url}.\n"
+                f"{e}"
+            )
+    else:
+        raise Exception(f"Failed to get public IP after {len(remote_ip_detection_hosts)} retries.")
 
-    def save_public_ip(self, public_ip: str) -> None:
-        log_path = self.config["ip_history_path"]
-        with open(log_path, "a") as ip_log:
-            ip_log.write(public_ip + "\n")
 
-    def get_previous_public_ip(self) -> str:
-        log_path = self.config["ip_history_path"]
-        try:
-            with open(log_path, "r") as ip_log:
-                return list(ip_log)[-1].strip()
-        except FileNotFoundError:
-            return ""
+def save_public_ip(public_ip: str) -> None:
+    log_path = environ["IP_HISTORY_PATH"]
+    with open(log_path, "a") as ip_log:
+        ip_log.write(public_ip + "\n")
+    logging.debug("Saved new public IP to file")
+
+
+def get_previous_public_ip() -> str:
+    log_path = environ["IP_HISTORY_PATH"]
+    try:
+        with open(log_path, "r") as ip_log:
+            previous_ip = list(ip_log)[-1].strip()
+            logging.debug(f"Previous IP: {previous_ip}")
+            return previous_ip
+    except FileNotFoundError:
+        logging.warning("No previous IP found")
+        return ""
+
+
+def resolve_dns(hostname: str) -> str:
+    resolved_ip = socket.gethostbyname(hostname)
+    logging.info(f"{hostname} resolved to {resolved_ip}")
+    return resolved_ip
