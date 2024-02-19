4390695858ce36ad8f419439219f83e97640ab17
Adam Żmuda <adzik007@gmail.com>
2024-02-19 18:47:38 +0100

    pre-commit lint changes (#9)
    * pre-commit config
    
    * fix all files
    
    * Dockerfiles
    
    * shell check
    
    * markdown lint
    
    * change dns description
    
    * quote in md
    
    * backup version

--- backup/vaultwarden.sh
+++ backup/vaultwarden.sh
@@ -6,5 +6,6 @@ BACKUP_FILE="vaultwarden-$(date "+%F--%H%M")"
 
 sqlite3 /data/db.sqlite3 ".backup '/tmp/db.sqlite3'"
 echo "$(date "+%F-%H:%M:%S") SQLite DB done"
-tar -czf - tmp/db.sqlite3 data/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /backup/${BACKUP_FILE}.tar.gz
+tar -czf - tmp/db.sqlite3 data/attachments \
+  | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:"${BACKUP_ENCRYPTION_KEY}" -out /backup/"${BACKUP_FILE}".tar.gz
 echo "$(date "+%F-%H:%M:%S") Files compressed and encrypted"
