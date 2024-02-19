5740c17d350329953136ba08303156e81ffd0b02
Adam Żmuda <adzik007@gmail.com>
2024-02-19 18:47:36 +0100

    skip update if ip didn't change

--- dns/src/public_ip.py
+++ dns/src/public_ip.py
@@ -3,11 +3,26 @@ import logging
 import requests
 
 
-def get_public_ip() -> str:
-    ip_url = "https://ifconfig.me"
-    public_ip = requests.request("GET", ip_url)
-    if public_ip.ok:
-        logging.info(f"PublicIP - {public_ip.text}")
-        return public_ip.text
-    else:
-        logging.error(f"Failed getting Public IP. Status code {public_ip.status_code}, Response: {public_ip.text}")
+class PublicIP:
+    def __init__(self, config: dict):
+        self.config = config
+
+    @staticmethod
+    def get_public_ip() -> str:
+        ip_url = "https://ifconfig.me"
+        public_ip = requests.request("GET", ip_url)
+        if public_ip.ok:
+            logging.info(f"Current Public IP - {public_ip.text}")
+            return public_ip.text
+        else:
+            logging.error(f"Failed getting Public IP. Status code {public_ip.status_code}, Response: {public_ip.text}")
+
+    def save_public_ip(self, public_ip: str) -> None:
+        log_path = self.config["ip_history_path"]
+        with open(log_path, "a") as ip_log:
+            ip_log.write(public_ip)
+
+    def get_previous_public_ip(self) -> str:
+        log_path = self.config["ip_history_path"]
+        with open(log_path, "r") as ip_log:
+            return list(ip_log)[-1]
--- dns/src/update.py
+++ dns/src/update.py
@@ -1,7 +1,9 @@
 import json
-from public_ip import get_public_ip
-import dns
 import logging
+
+from public_ip import PublicIP
+
+import dns
 
 
 def get_config(config_path: str) -> dict:
@@ -14,12 +16,18 @@ config = get_config("../config/config.json")
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s: %(message)s',
-    # filename='../logs/log',
     encoding='utf-8',
     level=config["log_level"]
 )
 
-public_ip = get_public_ip()
+ip_handler = PublicIP(config)
+
+current_ip = ip_handler.get_public_ip()
+previous_ip = ip_handler.get_previous_public_ip()
+
+if current_ip == previous_ip:
+    logging.info(f"IP Address unchanged")
+    quit(0)
 
 dns_token = dns.get_dns_token(
     username=secrets["username"],
@@ -30,12 +38,13 @@ dns_token = dns.get_dns_token(
 for entry in config["records"]:
     payload = dns.get_payload(
         template_path="../config/payload_template.json",
-        public_ip=public_ip, record_id=entry["id"],
+        public_ip=current_ip, record_id=entry["id"],
     )
     update_dns_status = dns.update_dns_entry(
         bearer_token=dns_token,
         device_token=secrets["device_token"],
         payload=payload,
     )
-    logging.info("Updated DNS record.")
+    logging.info(f"Updated DNS record. EntryID: {entry['id']}. PublicIP: {current_ip}")
     logging.debug(update_dns_status)
+    ip_handler.save_public_ip(public_ip=current_ip)
