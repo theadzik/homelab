9851511da086d5b9003d9f4474c25abbbfdcc496
Adam Żmuda <adzik007@gmail.com>
2024-08-28 22:34:15 +0200

    find a word

--- apps/reddit-meaningofwords/main.py
+++ apps/reddit-meaningofwords/main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+import json
+import logging
 import os
 
 import praw
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
 
 USER_AGENT = "linux:meaning-of-words:2024.08.1 (by u/MalinowyChlopak)"
 
@@ -14,6 +18,11 @@ CLIENT_SECRET = os.environ.get("CLIENT_SECRET", None)
 USERNAME = os.environ.get("USERNAME", None)
 PASSWORD = os.environ.get("PASSWORD", None)
 
+with open("dictionary.json", mode="r") as file:
+    dictionary = json.load(file)
+
+words_to_check = [word for word in dictionary.keys()]
+
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
@@ -21,3 +30,14 @@ reddit = praw.Reddit(
     password=PASSWORD,
     user_agent=USER_AGENT,
 )
+
+for comment in reddit.subreddit("all").stream.comments():
+    normalized_comment = comment.body.lower()
+
+    logging.debug(f"Checking comment {comment.id}")
+    for word in words_to_check:
+        if word in normalized_comment:
+            logging.info(comment.permalink)
+            logging.info(normalized_comment)
+
+# Got prawcore.exceptions.TooManyRequests
