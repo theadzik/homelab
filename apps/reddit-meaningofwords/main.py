309e9313c7ed016b81b8997a3dc5fdbdbe5bd0dd
Adam Żmuda <adzik007@gmail.com>
2024-08-29 22:42:42 +0200

    remove debug rate counter, update prompt, added new words

--- apps/reddit-meaningofwords/main.py
+++ apps/reddit-meaningofwords/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-import datetime
 import json
 import logging
 import os
+import unicodedata
 
 import praw
-import prawcore
 from dotenv import load_dotenv
 from openaihelper import openai_word_checker
 
@@ -21,7 +20,11 @@ class BotCommenter:
 
     @staticmethod
     def normalize_comment(body: str) -> str:
-        return body.lower()
+        body = body.lower()
+        # Remove Polish accents (ł is a special case)
+        body = body.replace("ł", "l")
+        body = unicodedata.normalize('NFKD', body).encode("ascii", "ignore").decode("ascii")
+        return body
 
     def find_keywords(self, body: str) -> str:
         for word in self.words_to_check:
@@ -62,32 +65,18 @@ reddit = praw.Reddit(
 
 bot_commenter = BotCommenter()
 
-counter = 0
-start_time = datetime.datetime.now()
-try:
-    for comment in reddit.subreddit(SUBREDDIT).stream.comments(skip_existing=True):
-        counter += 1
-        normalized_comment = bot_commenter.normalize_comment(comment.body)
+for comment in reddit.subreddit(SUBREDDIT).stream.comments(skip_existing=True):
+    normalized_comment = bot_commenter.normalize_comment(comment.body)
 
-        current_time = datetime.datetime.now()
-        current_seconds = (current_time - start_time).total_seconds()
-        logging.debug(f"Total counter: {counter} at rate: {(counter / current_seconds):.2f} req/s")
-        if keyword_found := bot_commenter.find_keywords(body=normalized_comment):
-            if comment.author.name == bot_commenter.bot_name:
-                logging.info("It's my own comment! Not Replying!")
-                continue
-            if response := openai_word_checker(body=comment.body, word=keyword_found):
-                logging.info("Found word misuse. Replying!")
-                response += f"\n\n{bot_commenter.signature}"
-                logging.info(response)
-                comment.reply(response)
-            else:
-                logging.info("Word used correctly. Skipping")
-                continue
-except prawcore.exceptions.TooManyRequests:
-    end_time = datetime.datetime.now()
-    end_seconds = (end_time - start_time).total_seconds()
-    logging.error(
-        f"TooManyRequests: {counter} iterations over {datetime.timedelta(seconds=end_seconds)},"
-        f"{(counter / end_seconds):.2f}req/s."
-    )
+    if keyword_found := bot_commenter.find_keywords(body=normalized_comment):
+        if comment.author.name == bot_commenter.bot_name:
+            logging.info("It's my own comment! Not Replying!")
+            continue
+        if response := openai_word_checker(body=comment.body, word=keyword_found):
+            logging.info("Replying.")
+            response += f"\n\n{bot_commenter.signature}"
+            logging.info(response)
+            comment.reply(response)
+        else:
+            logging.info("Not replying.")
+            continue
--- apps/reddit-meaningofwords/openaihelper.py
+++ apps/reddit-meaningofwords/openaihelper.py
@@ -10,17 +10,17 @@ def openai_word_checker(word: str, body: str) -> str or None:
         model="gpt-4o-mini",
         messages=[
             {"role": "system",
-             "content": "Próbuję wykryć czy prawidłowo używam paronimów, "
-                        "czyli podobnych wyrazów, ale znaczących coś innego. "
+             "content": "Próbuję wykryć czy prawidłowo używam słów"
                         "Podam ci słowo, które mogło, ale nie musiało być użyte błędnie. "
                         "Następnie podam ci fragment tekstu. "
                         "Sprawdź czy podane przeze mnie słowo było użyte poprawnie. Jeżeli tak, "
                         "odpowiedz słowem 'True',"
                         "jeżeli było użyte błędnie odpowiedz słowem 'False', "
                         "bez dodatkowej interpunkcji. "
-                        "W kolejnej linii uzasadnij swoją odpowiedź."
-                        "W kolejnej linii podaj słownikową definicję sprawdzanego słowa. "
+                        "W kolejnej linii uzasadnij swoją odpowiedź. "
                         "W kolejnej linii podaj poprawioną wersję zdania, które zawierało błędne użycie słowa. "
+                        "Popraw wszystkie błędy występujące w tym zdaniu. "
+                        "Zacznij tę linię od \"Poprawne zdanie:\""
                         "W ostatniej linii napisz tylko ciąg znaków 'meaningofwords', bez dodatkowej interpunkcji."
              },
             {"role": "system", "content": word},
@@ -33,7 +33,7 @@ def openai_word_checker(word: str, body: str) -> str or None:
     if content.endswith("meaningofwords"):
         if content.startswith("False"):
             logging.info("Incorrect usage found!")
-            response = "\n\n".join(content.split("\n")[1:4])
+            response = "\n\n".join(content.split("\n")[1:3])
             return response
         else:
             logging.info("Correct usage.")
