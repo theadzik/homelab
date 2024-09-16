50f82a579b957411eaab8c154034c71c8232cb78
Adam Żmuda <adzik007@gmail.com>
2024-09-16 21:01:06 +0200

    fastapi

--- apps/sentiment-analyzer/src/main.py
+++ apps/sentiment-analyzer/src/main.py
@@ -1,4 +1,22 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import pipeline
 
+
+class Item(BaseModel):
+    text: str
+
+
 nlp = pipeline("sentiment-analysis", model="bardsai/twitter-sentiment-pl-base")
-print(nlp("Która godzina?"))
+app = FastAPI()
+
+
+@app.post("/")
+def analyze_sentiment(body: Item):
+    body_dict = body.dict()
+    return nlp(body_dict["text"])[0]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
