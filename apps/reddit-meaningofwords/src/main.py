2a20d36175a465dbcbc5bf60c66555443611df3a
Adam Żmuda <adzik007@gmail.com>
2024-12-04 17:09:20 +0100

    Reddit bot - refactor, liveness probe, continue on reddit 5xx errors (#117)
    * break huge loop into smaller functions
    
    * Put the main loop into try except
    
    * create liveness file
    
    * add liveness probe to deploment
    
    * initial delay 10 minutes

--- apps/reddit-meaningofwords/src/main.py
+++ apps/reddit-meaningofwords/src/main.py
@@ -1,12 +1,13 @@
 import os
+import time
 
 import praw
 from bullying import BullyingClient
 from custom_logger import get_logger
 from database import DatabaseClientSingleton
 from dotenv import load_dotenv
-from graceful_shutdown import GracefulKiller
 from openai_helper import OpenAIChecker
+from prawcore import ServerError
 from reddit_bot import BotCommenter
 
 load_dotenv()
@@ -15,6 +16,7 @@ USER_AGENT = f"linux:{os.environ['REDDIT_USERNAME']}:{os.environ['APP_VERSION']}
 SUBREDDITS = os.getenv("REDDIT_SUBREDDITS", "polska")
 
 logger = get_logger(__name__)
+liveness_probe = os.getenv("LIVENESS_FILE_PATH", "/tmp/liveness")
 
 reddit = praw.Reddit(
     client_id=os.environ["REDDIT_CLIENT_ID"],
@@ -24,110 +26,127 @@ reddit = praw.Reddit(
     user_agent=USER_AGENT,
 )
 
-killer = GracefulKiller()
 bullying_analyzer = BullyingClient()
 database_client = DatabaseClientSingleton()
 
 logger.info(f"User-Agent: {USER_AGENT}")
 logger.info("Scanning comments.")
 
-for comment in reddit.subreddit(SUBREDDITS).stream.comments(skip_existing=True):
-    # Terminating process after a new comment is received, but before anything is done with it.
-    # This is because during upgrades there should already be an instance running when we get SIGTERM.
-    # It's better to not post at all than to double post.
-    if killer.kill_now:
-        logger.info("Received kill signal. Shutting down.")
-        break
 
+def handle_keyword(comment: praw.models.Comment, keyword_found: str, match: str) -> None:
+    bot_commenter = BotCommenter()
+    if comment.author.name == bot_commenter.bot_name:
+        logger.debug("It's my own comment! Skipping.")
+        return
+
+    if bot_commenter.is_my_comment_chain(comment):
+        logger.debug("It's a reply to my comment! Skipping.")
+        return
+
+    if bot_commenter.skip_comment(word=keyword_found):
+        logger.info(f"Skipping common word: {keyword_found}!")
+        database_client.increment_word_use(word=keyword_found, usage="skipped")
+        return
+
+    extra_info = bot_commenter.get_extra_info(keyword_found)
+    start_index, end_index = bot_commenter.get_sentence_indexes(word=match, body=comment.body, limit=2)
+    limited_body = bot_commenter.get_sentences(body=comment.body, start_index=start_index, end_index=end_index)
+
+    logger.debug(f"Limited comment body:\n{limited_body}")
+    logger.info(bot_commenter.REDDIT_BASE_URL + comment.permalink)
+    logger.info(f"Calling OpenAI for explanation: {keyword_found}")
+
+    # Initializing every time to update prompts without restarting.
+    openai_checker = OpenAIChecker()
+    content = openai_checker.get_explanation(body=limited_body, word=keyword_found, extra_info=extra_info)
+
+    if not content.is_correct:
+        logger.info(f"{content.used_word} used incorrectly. Replying!")
+        sources = bot_commenter.parse_reddit_sources(keyword_found)
+        response = bot_commenter.parse_reddit_explanation(content, sources)
+        _ = bot_commenter.reply_with_retry(comment=comment, reply=response)
+        database_client.increment_word_use(word=keyword_found, usage="incorrect_usage")
+    else:
+        logger.warning(f"{content.used_word} used correctly. Skipping.")
+        logger.warning(content)
+        database_client.increment_word_use(word=keyword_found, usage="correct_usage")
+
+
+def handle_direct_reply(comment: praw.models.Comment) -> None:
+    bot_commenter = BotCommenter()
+    logger.info(bot_commenter.REDDIT_BASE_URL + comment.permalink)
+    if bot_commenter.is_asking_for_block(comment=comment):
+        logger.warning(f"Blocking user {comment.author}")
+        reddit.redditor(comment.author).block()
+        database_client.save_bully(username=comment.author, banned=True)
+        return
+
+    if bot_commenter.is_bad_bot_comment(comment=comment):
+        if database_client.is_ghost(comment.author):
+            logger.info(f"{comment.author} is a ghost. Skipping.")
+            return
+        logger.warning("Bad bot detected :(")
+        openai_checker = OpenAIChecker()
+        content = openai_checker.get_bad_bot_response(comment.body)
+        _ = bot_commenter.reply_with_retry(comment=comment, reply=content.response)
+        database_client.save_ghost(comment.author, "Bad bot comment")
+        return
+
+    bullying_score = bullying_analyzer.get_bullying_prediction(text=comment.body)
+
+    if bullying_score["label"]:
+        logger.warning("Bullying detected :(")
+        logger.warning(f"{comment.body}")
+        if not database_client.is_warned_bully(comment.author):
+            openai_checker = OpenAIChecker()
+            content = openai_checker.get_bullying_response(comment.body)
+
+            if content.is_bullying:
+                logger.warning("First warning")
+                database_client.save_bully(comment.author)
+                _ = bot_commenter.reply_with_retry(comment=comment, reply=content.response)
+            else:
+                logger.warning("It wasn't bullying :D")
+                logger.warning(content)
+
+        else:
+            logger.warning(f"Second warning. Blocking bully {comment.author}")
+            reddit.redditor(comment.author).block()
+            database_client.save_bully(username=comment.author, banned=True)
+        return
+    else:
+        logger.info("No bullying :)")
+        logger.debug(f"{comment.body}")
+        return
+
+
+def handle_comment(comment: praw.models.Comment) -> None:
     # Initializing on every loop to reload dictionary without restarting.
     bot_commenter = BotCommenter()
     logger.debug(f"Found a comment: {comment.permalink}")
 
     if database_client.is_banned_bully(comment.author):
         logger.debug(f"Skipping comment from banned user {comment.author}")
-        continue
+        return
 
     # Check replies to my comments
     if bot_commenter.is_my_comment_chain(comment=comment, direct=True):
-        logger.info(bot_commenter.REDDIT_BASE_URL + comment.permalink)
-        if bot_commenter.is_asking_for_block(comment=comment):
-            logger.warning(f"Blocking user {comment.author}")
-            reddit.redditor(comment.author).block()
-            database_client.save_bully(username=comment.author, banned=True)
-            continue
-
-        if bot_commenter.is_bad_bot_comment(comment=comment):
-            if database_client.is_ghost(comment.author):
-                logger.info(f"{comment.author} is a ghost. Skipping.")
-                continue
-            logger.warning("Bad bot detected :(")
-            openai_checker = OpenAIChecker()
-            content = openai_checker.get_bad_bot_response(comment.body)
-            _ = bot_commenter.reply_with_retry(comment=comment, reply=content.response)
-            database_client.save_ghost(comment.author, "Bad bot comment")
-            continue
-
-        bullying_score = bullying_analyzer.get_bullying_prediction(text=comment.body)
-
-        if bullying_score["label"]:
-            logger.warning("Bullying detected :(")
-            logger.warning(f"{comment.body}")
-            if not database_client.is_warned_bully(comment.author):
-                openai_checker = OpenAIChecker()
-                content = openai_checker.get_bullying_response(comment.body)
-
-                if content.is_bullying:
-                    logger.warning("First warning")
-                    database_client.save_bully(comment.author)
-                    _ = bot_commenter.reply_with_retry(comment=comment, reply=content.response)
-                else:
-                    logger.warning("It wasn't bullying :D")
-                    logger.warning(content)
-
-            else:
-                logger.warning(f"Second warning. Blocking bully {comment.author}")
-                reddit.redditor(comment.author).block()
-                database_client.save_bully(username=comment.author, banned=True)
-            continue
-        else:
-            logger.info("No bullying :)")
-            logger.debug(f"{comment.body}")
-            continue
+        handle_direct_reply(comment=comment)
 
     keyword_found, match = bot_commenter.find_keywords(body=comment.body)
     if keyword_found:
-        if comment.author.name == bot_commenter.bot_name:
-            logger.debug("It's my own comment! Skipping.")
-            continue
+        handle_keyword(comment=comment, keyword_found=keyword_found, match=match)
 
-        if bot_commenter.is_my_comment_chain(comment):
-            logger.debug("It's a reply to my comment! Skipping.")
-            continue
 
-        if bot_commenter.skip_comment(word=keyword_found):
-            logger.info(f"Skipping common word: {keyword_found}!")
-            database_client.increment_word_use(word=keyword_found, usage="skipped")
-            continue
-
-        extra_info = bot_commenter.get_extra_info(keyword_found)
-        start_index, end_index = bot_commenter.get_sentence_indexes(word=match, body=comment.body, limit=2)
-        limited_body = bot_commenter.get_sentences(body=comment.body, start_index=start_index, end_index=end_index)
-
-        logger.debug(f"Limited comment body:\n{limited_body}")
-        logger.info(bot_commenter.REDDIT_BASE_URL + comment.permalink)
-        logger.info(f"Calling OpenAI for explanation: {keyword_found}")
-
-        # Initializing every time to update prompts without restarting.
-        openai_checker = OpenAIChecker()
-        content = openai_checker.get_explanation(body=limited_body, word=keyword_found, extra_info=extra_info)
-
-        if not content.is_correct:
-            logger.info(f"{content.used_word} used incorrectly. Replying!")
-            sources = bot_commenter.parse_reddit_sources(keyword_found)
-            response = bot_commenter.parse_reddit_explanation(content, sources)
-            _ = bot_commenter.reply_with_retry(comment=comment, reply=response)
-            database_client.increment_word_use(word=keyword_found, usage="incorrect_usage")
-        else:
-            logger.warning(f"{content.used_word} used correctly. Skipping.")
-            logger.warning(content)
-            database_client.increment_word_use(word=keyword_found, usage="correct_usage")
+# Main loop
+while True:
+    try:
+        for comment in reddit.subreddit(SUBREDDITS).stream.comments(skip_existing=True):
+            # Create a file for liveness probe
+            with open(liveness_probe, mode='a'):
+                pass
+            handle_comment(comment=comment)
+    except ServerError as e:
+        logger.error(e)
+        logger.info("Waiting 60 seconds.")
+        time.sleep(60)
